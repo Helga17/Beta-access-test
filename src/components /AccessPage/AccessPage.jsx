570f5f2de24914c11736519e4d5a55643dbdfe19
Helga <1999olechka@gmail.com>
2021-05-17 23:07:05 +0300

    change the name of a variable

--- src/components /AccessPage/AccessPage.jsx
+++ src/components /AccessPage/AccessPage.jsx
@@ -6,7 +6,7 @@ import './AccessPage.css';
 const AccessPage = (props) => {
 
     const [codesData, setCodesData] = useState([]);
-    const [codeUser, setCodeUser] = useState('');
+    const [codeValue, setCodeValue] = useState('');
 
     useEffect(() => {
         axios.get('https://607ed59302a23c0017e8c2c6.mockapi.io/api/codes')
@@ -16,11 +16,11 @@ const AccessPage = (props) => {
     }, []);
 
     const inputChange = (event) => {
-        setCodeUser(event.target.value);
+        setCodeValue(event.target.value);
     }
 
     const onSubmit = (event) => {
-        let findCode = codesData.find(item => item.code === codeUser);
+        let findCode = codesData.find(item => item.code === codeValue);
 
         if(findCode) {
             const user = {firstName: 'Olya', lastName: 'Shcherbakova'};
@@ -38,7 +38,7 @@ const AccessPage = (props) => {
             </div>
             {!props.currentUser &&
             <div className="box">
-                <input type="text" className="form-control" value={codeUser} onChange={inputChange} placeholder="Enter the code" />
+                <input type="text" className="form-control" value={codeValue} onChange={inputChange} placeholder="Enter the code" />
 
                 <button className="btn btn-primary" type="submit" onClick={(event) => onSubmit(event)}>Submit</button>
             </div>} 
